9eb16539451a1f7b7fdb1d660e1447f6d57762d9
b0518n <b0518n@naver.com>
2023-08-07 18:56:36 +0900

    Add NamedPipeServer and NamedPipeClient

---
+++ NamedPipeClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO.Pipes;
+
+namespace NamedPipeClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "mypipe", PipeDirection.InOut))
+                {
+                    Console.WriteLine("클라이언트 : 서버에 연결을 시도 중...");
+                    pipeClient.Connect();
+                    Console.WriteLine("클라이언트 : 서버에 연결 성공!");
+
+                    string message;
+                    string response;
+
+                    using (StreamReader sr = new StreamReader(pipeClient))
+                    using (StreamWriter sw = new StreamWriter(pipeClient))
+                    {
+                        while(true)
+                        {
+                            Console.WriteLine("클라이언트 : 메시지 입력 (exit로 종료)");
+                            message = Console.ReadLine();
+                            if (message == "exit")
+                            {
+                                break;
+                            }
+
+                            sw.WriteLine(message);
+                            sw.Flush();
+
+                            response = sr.ReadLine();
+                            Console.WriteLine($"클라이언트 : 받은 응답 - {response}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"클라이언트 에러 : {ex.Message}");
+            }
+
+            Console.WriteLine("클라이언트 : 종료됨");
+        }
+    }
+}
---
+++ NamedPipeServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Pipes;
+
+namespace NamedPipeServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // PipeDirection : 파이프의 방향을 지정
+                // PipeDirection.In : 읽기 전용(단방향)
+                // PipeDirection.Out : 쓰기 전용(단방향)
+                // PipeDirection.InOut : 양방향
+                using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("mypipe", PipeDirection.InOut))
+                {
+                    Console.WriteLine("서버 : 파이프를 생성하고 클라이언트를 기다리는 중...");
+                    pipeServer.WaitForConnection(); // 동기식
+                    Console.WriteLine("서버 : 클라이언트와 연결 성공!");
+
+                    string message;
+                    string response;
+
+                    using (StreamReader sr = new StreamReader(pipeServer))
+                    using (StreamWriter sw = new StreamWriter(pipeServer))
+                    {
+                        while (true)
+                        {
+                            message = sr.ReadLine();
+                            if (message == "exit")
+                            {
+                                break;
+                            }
+
+                            Console.WriteLine($"서버 : 받은 메시지 - {message}");
+
+                            // 서버에서 클라이언트로 메시지 전송
+                            response = "서버가 받은 메시지: " + message;
+                            sw.WriteLine(response);
+                            sw.Flush();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"서버 에러: {ex.Message}");
+            }
+
+            Console.WriteLine("서버 : 종료됨");
+        }
+    }
+}
